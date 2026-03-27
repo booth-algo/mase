e272e3f90d404a15224a4f116f13645b724c9d82
Dimitris Alexopoulos <glumfishfish@gmail.com>
2026-03-27 23:26:52 +0200

    Add testbench flag for pipelined components

--- src/mase_components/dwn_layers/test/integration/run_top_tb.py
+++ src/mase_components/dwn_layers/test/integration/run_top_tb.py
@@ -26,7 +26,7 @@ def forward_env():
     env.update({key: value for key in ('CUDA_HOME', 'TORCH_CUDA_ARCH_LIST', 'TORCH_EXTENSIONS_DIR') if (value := os.environ.get(key))})
     return env
 
-def top_tb_runner(model_name, full, no_emit):
+def top_tb_runner(model_name, full, pipelined, no_emit):
     model_path = model_dir / f"{model_name}.pt"
     sim = os.getenv('SIM', 'verilator')
     runner = get_runner(sim) 
@@ -39,10 +39,13 @@ def top_tb_runner(model_name, full, no_emit):
         if os.path.exists(rtl_dir):
             shutil.rmtree(rtl_dir)
         os.mkdir(rtl_dir)
-        emit_dwn_rtl(ckpt_path=model_path, output_dir=rtl_dir, full_pipeline=True)
+        emit_dwn_rtl(ckpt_path=model_path, output_dir=rtl_dir, full_pipeline=pipelined)
+    if full:
+        hdl_toplevel = 'full_pipeline_top_clocked' if pipelined else 'full_pipeline_top'
+    else:
+        hdl_toplevel = 'dwn_top_clocked' if pipelined else 'dwn_top'
     runner.build(
-        # hdl_toplevel='full_pipeline_top' if full else 'dwn_top',
-        hdl_toplevel = 'full_pipeline_top_clocked',
+        hdl_toplevel = hdl_toplevel,
         # TODO: Switch back to this one RTL generation is finalized
         # verilog_sources=[
         #     rtl_path /'dwn_top.sv',
@@ -63,8 +66,7 @@ def top_tb_runner(model_name, full, no_emit):
     
     runner.test(
         test_module='top_tb',
-        # hdl_toplevel='full_pipeline_top' if full else 'dwn_top',
-        hdl_toplevel = 'full_pipeline_top_clocked',
+        hdl_toplevel = hdl_toplevel,
         waves=True,
         extra_env={'MODEL_PATH': str(model_path), 'MODEL_MODE': 'full' if full else 'core'} | forward_env()
     )
@@ -72,7 +74,8 @@ def top_tb_runner(model_name, full, no_emit):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--full', action='store_true', help='Run the testbench with all RTL components (thermometer and groupsum)')
+    parser.add_argument('--pipelined', action='store_true', help='Run the testbench with the pipelined RTL components')
     parser.add_argument('--no-emit', action='store_true', help='Do not emit RTL')
     parser.add_argument('--model', help='Model checkpoint name')
     args = parser.parse_args()
-    top_tb_runner(args.model, args.full, args.no_emit)
+    top_tb_runner(args.model, args.full, args.pipelined, args.no_emit)
